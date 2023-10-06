c6b0818526cf46abb2bda4dd5c9b46b4c1506639
Will Bordman <willbordman@pm.me>
2023-10-06 15:07:28 -0400

    changed directory structure & updated bot.py
    Created a separate folder for game modes. Bot now has a new game menu.

--- bot.py
+++ bot.py
@@ -1,27 +1,56 @@
 import discord
-import responses
+from discord.ext import commands
+#import responses
 
+'''
 async def send_message(message, user_message, is_private):
     try:
         response = responses.handle_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
+'''
 
+# container for gamemode buttons
+class GamemodeSelectionButtons(discord.ui.View):
+        def __init__(self, *, timeout=180):
+            super().__init__(timeout=timeout)
+        @discord.ui.button(label="Standard Wordle",style=discord.ButtonStyle.primary)
+        async def standard_wordle_button(self,interaction:discord.Interaction,button:discord.ui.Button):
+            await interaction.response.edit_message(content=f"Selected Standard Wordle")
+        @discord.ui.button(label="Daily Wordle",style=discord.ButtonStyle.primary)
+        async def daily_wordle_button(self,interaction:discord.Interaction,button:discord.ui.Button):
+            await interaction.response.edit_message(content=f"Selected Daily Wordle")
+        @discord.ui.button(label="Feudle",style=discord.ButtonStyle.primary)
+        async def feudle_button(self,interaction:discord.Interaction,button:discord.ui.Button):
+            await interaction.response.edit_message(content=f"Selected Feudle")
+        @discord.ui.button(label="Multiplayer Wordle",style=discord.ButtonStyle.primary)
+        async def multiplayer_wordle_button(self,interaction:discord.Interaction,button:discord.ui.Button):
+            await interaction.response.edit_message(content=f"Selected Multiplayer Wordle")
 
+# start the bot
 def run_discord_bot():
     TOKEN = "MTE1NjYwMDYzNjU0NTE3OTY3OQ.G9AnaM.VdVSNB_1w1h2Lwq-u3Dam4wI2HsX31UzLtaHEA"
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intents)
-
-    @client.event
+    bot = commands.Bot(command_prefix='!', intents=intents)
+    
+    # bot startup greeting
+    @bot.event
     async def on_ready():
-        print(f'{client.user} is running!')
+        print(f'{bot.user} is running!')
 
-    @client.event
+    # !newgame command handler
+    @bot.command(name="newgame")
+    async def _newgame(ctx):
+        await ctx.send(
+            "Select a gamemode:",view=GamemodeSelectionButtons()
+        )
+    
+    '''
+    @bot.event
     async def on_message(message):
-        if message.author == client.user:
+        if message.author == bot.user:
             return
         username = str(message.author)
         user_message = str(message.content)
@@ -34,7 +63,6 @@ def run_discord_bot():
             await send_message(message, user_message, True)
         else:
             await send_message(message, user_message, False)
+    '''
 
-
-
-    client.run(TOKEN)
+    bot.run(TOKEN)
---
+++ gamemodes/standard_wordle.py
@@ -0,0 +1,2 @@
+def standard_wordle():
+    pass
